3c867173796c1fb206b4f9eb9afb01064df54389
Ayush Adhikari <ayush.adhikari1203@gmail.com>
2026-04-20 02:47:21 -0400

    removed template code, made a simple streamlit shell for testing, set up docker containers (API, Streamlit, DB)

---
+++ database-files/01_gamegoats_bootstrap.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS gamegoats_db;
+CREATE DATABASE IF NOT EXISTS gamegoats_db;
+USE gamegoats_db;
+
+-- Phase 1 bootstrap only. Will implement the rest of the tables and relationships later.
